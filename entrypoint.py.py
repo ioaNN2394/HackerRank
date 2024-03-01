78411c3faa641227654945441fe4fbeaffbce660
Santid21 <138895787+Santid21@users.noreply.github.com>
2024-03-01 10:27:36 -0500

    Add files via upload

---
+++ entrypoint.py.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    suma = a + b
+    diferencia = a - b
+    producto = a * b
+
+print(suma)
+print(diferencia)
+print(producto)
+
+
